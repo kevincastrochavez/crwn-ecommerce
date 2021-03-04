678195e305b52a06688c6a170119fe563f2fb56b
kevincastrochavez <kevin.castro.chavez0@gmail.com>
2021-03-03 19:52:24 -0600

    refactored code and separated into smaller components with proper architecture. Did some styles

---
+++ src/pages/homepage/Homepage.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Directory from "../../components/directory/directory";
+
+import "./homepage.scss";
+
+function Homepage() {
+  return (
+    <div className="homepage">
+      <Directory />
+    </div>
+  );
+}
+
+export default Homepage;
