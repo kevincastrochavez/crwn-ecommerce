5497000978101b89cf00dca1cfc3d475b08eb767
kevincastrochavez <kevin.castro.chavez0@gmail.com>
2021-03-04 15:59:38 -0600

    created the shopPage, the collection-preview component and its stylesheet. Render the items in the collection-preview and filtered them

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React from "react";
+import { Route, Switch } from "react-router-dom";
+
 import Homepage from "./pages/homepage/Homepage";
+import ShopPage from "./pages/shop/ShopPage";
 
 import "./App.css";
 
 function App() {
   return (
-    <div className="app">
-      <Homepage />
+    <div>
+      <Switch>
+        <Route exact path="/" component={Homepage} />
+        <Route exact path="/shop" component={ShopPage} />
+      </Switch>
     </div>
   );
 }
