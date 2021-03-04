1a91821e7afe0075a1be0df821b326a603756539
kevincastrochavez <kevin.castro.chavez0@gmail.com>
2021-03-04 17:24:30 -0600

    created the header component, included a logo and some links

--- src/components/collection-preview/collection-preview.jsx
+++ src/components/collection-preview/collection-preview.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import CollectionItem from "../collection-item/collection-item";
 import "./collection-preview.scss";
 
 function CollectionPreview({ title, items }) {
@@ -8,8 +9,8 @@ function CollectionPreview({ title, items }) {
       <div className="preview">
         {items
           .filter((item, index) => index < 4)
-          .map((item) => (
-            <div key={item.id}>{item.name}</div>
+          .map(({ id, ...otherItemProps }) => (
+            <CollectionItem key={id} {...otherItemProps} />
           ))}
       </div>
     </div>
--- src/pages/shop/ShopPage.jsx
+++ src/pages/shop/ShopPage.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import CollectionPreview from "../../components/preview-collection/collection-preview";
+import CollectionPreview from "../../components/collection-preview/collection-preview";
 import SHOP_DATA from "./shop-data";
 
 class ShopPage extends Component {
