f94c8a7fed589194f43783c1b3e58d6552438d1e
kevincastrochavez <kevin.castro.chavez0@gmail.com>
2021-03-11 17:54:31 -0600

    created the action ADD_ITEM to add items to the state

--- src/components/collection-item/collection-item.jsx
+++ src/components/collection-item/collection-item.jsx
@@ -1,10 +1,14 @@
 import React from "react";
+import { connect } from "react-redux";
+import { addItem } from "../../redux/cart/cartActions";
 
 import CustomButton from "../custom-button/custom-button";
 
 import "./collection-item.scss";
 
-function CollectionItem({ id, name, price, imageUrl }) {
+function CollectionItem({ item, addItem }) {
+  const { name, price, imageUrl } = item;
+
   return (
     <div className="collection-item">
       <div className="image" style={{ backgroundImage: `url(${imageUrl})` }} />
@@ -13,9 +17,15 @@ function CollectionItem({ id, name, price, imageUrl }) {
         <span className="price">{price}</span>
       </div>
 
-      <CustomButton inverted>Add to Cart</CustomButton>
+      <CustomButton onClick={() => addItem(item)} inverted>
+        Add to Cart
+      </CustomButton>
     </div>
   );
 }
 
-export default CollectionItem;
+const mapDispatchToProps = (dispatch) => ({
+  addItem: (item) => dispatch(addItem(item)),
+});
+
+export default connect(null, mapDispatchToProps)(CollectionItem);
