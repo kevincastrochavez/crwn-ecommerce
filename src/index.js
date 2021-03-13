e693df4b60224c5090fc17d84ed8d579f98a4a2f
kevincastrochavez <kevin.castro.chavez0@gmail.com>
2021-03-13 13:41:47 -0600

    installed redux persist and set up application to persist the state into LocalStorage with redux

--- src/index.js
+++ src/index.js
@@ -2,9 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
 
 import App from "./App";
-import store from "./redux/store";
+import { store, persistor } from "./redux/store";
 import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
 import reportWebVitals from "./reportWebVitals";
 
@@ -14,7 +15,9 @@ ReactDOM.render(
   <Provider store={store}>
     <React.StrictMode>
       <BrowserRouter>
-        <App />
+        <PersistGate persistor={persistor}>
+          <App />
+        </PersistGate>
       </BrowserRouter>
     </React.StrictMode>
   </Provider>,
