a745b1eb22b8d8f2589ea021e3740c086c291069
kevincastrochavez <kevin.castro.chavez0@gmail.com>
2021-03-16 10:50:34 -0600

    implemented data normalization with the shopData file

--- src/pages/collection/collection.jsx
+++ src/pages/collection/collection.jsx
@@ -1,14 +1,21 @@
 import React from "react";
 import { connect } from "react-redux";
+import CollectionItem from "../../components/collection-item/collection-item";
 import { selectCollection } from "../../redux/shop/shopSelectors";
 
 import "./collection.scss";
 
 function Collection({ collection }) {
-  console.log(collection);
+  const { title, items } = collection;
+
   return (
     <div className="collection">
-      <h2>COLLECTION PAGE</h2>
+      <h2 className="title">{title}</h2>
+      <div className="items">
+        {items.map((item) => (
+          <CollectionItem key={item.id} item={item} />
+        ))}
+      </div>
     </div>
   );
 }
