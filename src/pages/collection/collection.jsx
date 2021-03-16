93cc93abfcbe35e47114f8506d188abf056beec4
kevincastrochavez <kevin.castro.chavez0@gmail.com>
2021-03-16 10:15:24 -0600

    changed the name of category to collection in all the instances used

---
+++ src/pages/collection/collection.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+import "./collection.scss";
+
+function Collection({ match }) {
+  console.log(match);
+  return (
+    <div className="collection">
+      <h2>COLLECTION PAGE</h2>
+    </div>
+  );
+}
+
+export default Collection;
