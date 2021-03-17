4100514835ebfc01e15551ef71b0f26dd69b7de3
kevincastrochavez <kevin.castro.chavez0@gmail.com>
2021-03-17 11:29:36 -0600

    got the well structured information for the shop page from the collections firestore

--- src/pages/shop/ShopPage.jsx
+++ src/pages/shop/ShopPage.jsx
@@ -1,16 +1,33 @@
-import React from "react";
+import React, { Component } from "react";
 import { Route } from "react-router";
 
 import CollectionsOverview from "../../components/collections-overview/collections-overview";
+import {
+  firestore,
+  convertCollectionsSnapshotToMap,
+} from "../../firebase/firebase-utils";
 import Collection from "../collection/collection";
 
-const ShopPage = ({ match }) => {
-  return (
-    <div className="shop-page">
-      <Route exact path={`${match.path}`} component={CollectionsOverview} />
-      <Route path={`${match.path}/:collectionId`} component={Collection} />
-    </div>
-  );
-};
+class ShopPage extends Component {
+  unsuscribeFromSnapshot = null;
+
+  componentDidMount() {
+    const collectionRef = firestore.collection("collections");
+
+    collectionRef.onSnapshot(async (snapshot) => {
+      convertCollectionsSnapshotToMap(snapshot);
+    });
+  }
+
+  render() {
+    const { match } = this.props;
+    return (
+      <div className="shop-page">
+        <Route exact path={`${match.path}`} component={CollectionsOverview} />
+        <Route path={`${match.path}/:collectionId`} component={Collection} />
+      </div>
+    );
+  }
+}
 
 export default ShopPage;
