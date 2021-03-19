754b74235c19b4a07345e7ff22805f7019fd94d8
kevincastrochavez <kevin.castro.chavez0@gmail.com>
2021-03-18 18:27:44 -0600

    converted the subscription observable patter to a promise pattern

--- src/pages/shop/ShopPage.jsx
+++ src/pages/shop/ShopPage.jsx
@@ -25,12 +25,19 @@ class ShopPage extends Component {
     const { updateCollections } = this.props;
     const collectionRef = firestore.collection("collections");
 
-    collectionRef.onSnapshot(async (snapshot) => {
+    collectionRef.get().then((snapshot) => {
       const collectionsMap = convertCollectionsSnapshotToMap(snapshot);
 
       updateCollections(collectionsMap);
       this.setState({ loading: false });
     });
+
+    // collectionRef.onSnapshot(async (snapshot) => {
+    //   const collectionsMap = convertCollectionsSnapshotToMap(snapshot);
+
+    //   updateCollections(collectionsMap);
+    //   this.setState({ loading: false });
+    // });
   }
 
   render() {
