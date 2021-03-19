c4a97a253f12f838c88e7378f6a653df97e1b09f
kevincastrochavez <kevin.castro.chavez0@gmail.com>
2021-03-19 13:16:46 -0600

    migrated functionality of fetchingCollections with redux to functions generators with sagas

--- src/redux/shop/shopSagas.js
+++ src/redux/shop/shopSagas.js
@@ -1,8 +1,27 @@
-import { takeEvery } from "redux-saga/effects";
+import { call, put, takeEvery } from "redux-saga/effects";
+
 import ShopActionTypes from "./shopTypes";
+import {
+  convertCollectionsSnapshotToMap,
+  firestore,
+} from "../../firebase/firebase-utils";
+import {
+  fetchCollectionsFailure,
+  fetchCollectionsSuccess,
+} from "./shopActions";
 
 export function* fetchCollectionsAsync() {
-  yield console.log("Fired");
+  try {
+    const collectionRef = firestore.collection("collections");
+    const snapshot = yield collectionRef.get();
+    const collectionsMap = yield call(
+      convertCollectionsSnapshotToMap,
+      snapshot
+    );
+    yield put(fetchCollectionsSuccess(collectionsMap));
+  } catch (error) {
+    yield put(fetchCollectionsFailure(error.message));
+  }
 }
 
 export function* fetchCollectionsStart() {
