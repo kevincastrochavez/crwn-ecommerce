483e96f642a1813c9b8632128a3a9cd09e0f77e1
kevincastrochavez <kevin.castro.chavez0@gmail.com>
2021-03-19 13:49:09 -0600

    created a root sagato be able to call there all the sagas

--- src/redux/shop/shopSagas.js
+++ src/redux/shop/shopSagas.js
@@ -1,4 +1,4 @@
-import { call, put, takeEvery } from "redux-saga/effects";
+import { call, put, takeLatest } from "redux-saga/effects";
 
 import ShopActionTypes from "./shopTypes";
 import {
@@ -25,7 +25,7 @@ export function* fetchCollectionsAsync() {
 }
 
 export function* fetchCollectionsStart() {
-  yield takeEvery(
+  yield takeLatest(
     ShopActionTypes.FETCH_COLLECTIONS_START,
     fetchCollectionsAsync
   );
