2bb209de3d3732f640fc8884169eadcc2c23cc34
kevincastrochavez <kevin.castro.chavez0@gmail.com>
2021-03-31 11:31:47 -0600

    used react memo to memoize the cartItem and not re-render it when the object doesn't change

--- client/src/components/cart-item/cart-item.jsx
+++ client/src/components/cart-item/cart-item.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { memo } from "react";
 
 import "./cart-item.scss";
 
@@ -17,4 +17,4 @@ function CartItem({ item: { imageUrl, price, name, quantity } }) {
   );
 }
 
-export default CartItem;
+export default memo(CartItem);
--- client/src/pages/homepage/Homepage.jsx
+++ client/src/pages/homepage/Homepage.jsx
@@ -4,8 +4,6 @@ import Directory from "../../components/directory/directory";
 import { HomepageContainer } from "./HomepageStyles";
 
 function Homepage() {
-  throw Error;
-
   return (
     <HomepageContainer>
       <Directory />
